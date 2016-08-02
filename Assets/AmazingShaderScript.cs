3f1d5165c87cf2d08c0976fb3045db8cce5239a3
Hümmer, Simon <simon.huemmer@student.fhws.de>
2016-08-02 10:13:28 +0200

    shader for fiddeling about (AmazingShader) is now in Project

---
+++ Assets/AmazingShaderScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class AmazingShaderScript : MonoBehaviour {
+
+    public Material material;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+
+
+        Graphics.Blit(source,destination,material);
+
+    }
+
+}
