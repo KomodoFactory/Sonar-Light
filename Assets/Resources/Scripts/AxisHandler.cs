a9392619da8483fd04d0dd422c82cfe3217185de
Domi <dominik.rose@fhws.de>
2016-08-19 11:46:36 +0200

    controller support now working

---
+++ Assets/Resources/Scripts/AxisHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxisHandler{
+
+    public string axisTooCheck = "JostickButton0";
+    float lastState = 0;
+    float newState = 0;
+
+    public AxisHandler(string axisTooCheck)
+    {
+        this.axisTooCheck = axisTooCheck;
+    }
+
+    public void Update()
+    {
+        lastState = newState;
+        newState = Input.GetAxis(axisTooCheck);
+    }
+
+
+    public bool pressedDown()
+    {
+        return (lastState == 0 && newState == 1);
+    }
+
+    public bool pressed()
+    {
+        return (newState == 1);
+    }
+
+    public bool released()
+    {
+        return (lastState == 1 && newState == 0);
+    }
+
+}
