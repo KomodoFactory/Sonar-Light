c5dce9f5e1c878dd4889f582f4e3b3b57d39bb2e
Domi <dominik.rose@fhws.de>
2016-08-25 15:15:26 +0200

    implemented constant edge detection by developer console

---
+++ Assets/Resources/Scripts/Shader/MaterialHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MaterialHandler : MonoBehaviour
+{
+
+    public Material nonShaderMaterial;
+    public Material ShaderMaterial;
+    private static MaterialHandler instance;
+
+    private MaterialHandler() { }
+
+    public static MaterialHandler getInstance()
+    {
+        if (instance == null)
+        {
+            instance = GameObject.FindGameObjectWithTag("Console").GetComponent<MaterialHandler>();
+        }
+        return instance;
+    }
+
+    public bool switchMaterial(Renderer renderer)
+    {
+        Debug.Log("Shader: " + ShaderMaterial);
+        Debug.Log("Nonshader: " + nonShaderMaterial);
+
+        if (renderer.sharedMaterial == nonShaderMaterial)
+        {
+            renderer.sharedMaterial = ShaderMaterial;
+            return true;
+        }
+        else
+        {
+            renderer.sharedMaterial = nonShaderMaterial;
+            return false;
+        }
+    }
+
+    public void setShaderData(List<Sound> sounds)
+    {
+
+    }
+}
