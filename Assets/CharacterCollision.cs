45453bf24f4055dbf906b75a0888ee0d28b45562
david.kurbel <d.kurbel@web.de>
2016-08-26 09:51:17 +0200

    Removed debug lines from CaracterCollision

--- Assets/CharacterCollision.cs
+++ Assets/CharacterCollision.cs
@@ -11,11 +11,9 @@ public class CharacterCollision : MonoBehaviour {
     Vector3 colDir;
     // Use this for initialization
     void Start() {
-        Debug.Log("Initialize");
         etho = GameObject.FindGameObjectWithTag("Player");
         playerTransform = etho.transform;
         playerPos = playerTransform.position;
-        //playerVel = etho.GetComponent<CharacterMotor>().inputMoveDirection;
     }
 	
 	// Update is called once per frame
@@ -31,28 +29,9 @@ public class CharacterCollision : MonoBehaviour {
             Vector3 closestPointPlayer = etho.GetComponent<Collider>().ClosestPointOnBounds(col.transform.position);
             Vector3 closestPointOther = col.ClosestPointOnBounds(playerPos);
             colDir = closestPointOther - closestPointPlayer;
-            //Debug.Log("DirVec: " + colDir);
-            //Debug.Log("PlayerVel " + playerVel.magnitude);
             Vector3 force = colDir.normalized * collisionForce;
             Debug.Log(force);
             col.gameObject.GetComponent<Rigidbody>().AddRelativeForce(force);
         }
     }
-
-    //void OnCollisionEnter(Collision col)
-    //{
-    //    Debug.Log("Collisions WOW");
-    //    ContactPoint[] colPoints = col.contacts;
-    //    foreach (ContactPoint colPoint in colPoints)
-    //    {
-    //        Vector3 colPointVec = colPoint.point;
-    //        if (col.gameObject.GetComponent<Rigidbody>() != null)
-    //        {
-    //            colDir = colPointVec - playerPos;
-    //            Vector3 force = colDir.normalized * 100000;
-    //            Debug.Log(force);
-    //            col.gameObject.GetComponent<Rigidbody>().AddRelativeForce(colDir.normalized * 100000);
-    //        }
-    //    }
-    //}
 }
