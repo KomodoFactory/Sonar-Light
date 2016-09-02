0c0484ee0a76fd3155e7733356c3b1e24e38ba28
Hümmer, Simon <simon.huemmer@student.fhws.de>
2016-09-02 15:12:01 +0200

    added FOV command

---
+++ Assets/Resources/Scripts/UnityConsole-master/Console/Scripts/Commands/FovCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Wenzil.Console.Commands {
+    public static class FovCommand {
+
+        public static readonly string name = "FOV";
+        public static readonly string description = "Changes the FOV";
+        public static readonly string usage = "FOV <angle>";
+
+        public static string Execute(params string[] args) {
+
+            Camera camera = Camera.main;
+            if(args.Length != 1) {
+                return usage;
+            }
+            try {
+
+                int fov = Int32.Parse(args[0]);
+                if(fov < 10 || fov > 170) {
+                    return "please select a reasonable FOV, usually 60-100 look pretty okay";
+                }
+                camera.fieldOfView = fov;
+            }
+            catch(FormatException e) {
+                return e.Message;
+            }
+            return "Your FOV is now " + args[0];
+        }
+    }
+}
--- Assets/Resources/Scripts/UnityConsole-master/Console/Scripts/DefaultCommands.cs
+++ Assets/Resources/Scripts/UnityConsole-master/Console/Scripts/DefaultCommands.cs
@@ -1,12 +1,9 @@
 ﻿using UnityEngine;
 using Wenzil.Console.Commands;
 
-namespace Wenzil.Console
-{ 
-    public class DefaultCommands : MonoBehaviour
-    {
-        void Start()
-        {
+namespace Wenzil.Console {
+    public class DefaultCommands : MonoBehaviour {
+        void Start() {
             ConsoleCommandsDatabase.RegisterCommand(QuitCommand.name, QuitCommand.description, QuitCommand.usage, QuitCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(HelpCommand.name, HelpCommand.description, HelpCommand.usage, HelpCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(LoadCommand.name, LoadCommand.description, LoadCommand.usage, LoadCommand.Execute);
@@ -14,8 +11,9 @@ namespace Wenzil.Console
             ConsoleCommandsDatabase.RegisterCommand(MetaCommand.name, MetaCommand.description, MetaCommand.usage, MetaCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(OutlineCommand.name, OutlineCommand.description, OutlineCommand.usage, OutlineCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(ShaderCommand.name, ShaderCommand.description, ShaderCommand.usage, ShaderCommand.Execute);
-            ConsoleCommandsDatabase.RegisterCommand(ResetLevelCommand.name,ResetLevelCommand.description,ResetLevelCommand.usage,ResetLevelCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(ResetLevelCommand.name, ResetLevelCommand.description, ResetLevelCommand.usage, ResetLevelCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(SpawnObjectCommand.name, SpawnObjectCommand.description, SpawnObjectCommand.usage, SpawnObjectCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(FovCommand.name, FovCommand.description, FovCommand.usage, FovCommand.Execute);
         }
     }
 }
