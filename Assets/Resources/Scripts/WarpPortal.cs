f180dd0c030810e01a34ae383ed5a2410f440677
Hümmer, Simon <simon.huemmer@student.fhws.de>
2016-09-09 10:58:13 +0200

    started working on the warp portals again because i apperently never commited them. also changed the mus prefab

---
+++ Assets/Resources/Scripts/WarpPortal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider))]
+public class WarpPortal : MonoBehaviour {
+
+    public SceneField scene;
+
+    void onTriggerEnter(Collider other) {
+
+        Debug.Log(other);
+
+        if (other.gameObject.tag.Equals("Player")) {
+
+            if (scene != null) {
+                SceneManager.LoadScene(scene, LoadSceneMode.Single);
+            }else {
+                Debug.LogError("No Scene to Load!");
+            }
+
+        }
+
+
+    }
+        
+}
