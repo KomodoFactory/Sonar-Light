1003f0d37e7ce5cf73189d72917975ef1b52e130
Hümmer, Simon <simon.huemmer@student.fhws.de>
2016-09-27 12:30:09 +0200

    added scenes for the presentation

---
+++ Assets/Impact_Test_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Impact_Test_Script : MonoBehaviour {
+
+    
+    bool show = true;
+    public GameObject destroyMe;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+        if (show) {
+            ScreenPromptHandler.Instance.DisplayPrompt("F8 to Start", 4);
+            show = false;
+        }
+        if (Input.GetKeyDown(KeyCode.F8)) {
+            GameObject.Destroy(destroyMe);
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+
+	
+	}
+}
